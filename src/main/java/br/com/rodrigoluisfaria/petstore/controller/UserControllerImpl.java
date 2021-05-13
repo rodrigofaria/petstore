f7c13c93759f369e83a98d0de658d78df082a7c2
Rodrigo Faria <rodrigo.faria@nexadigital.com.br>
2021-05-13 16:48:03 -0300

    Add log to controller layer

--- src/main/java/br/com/rodrigoluisfaria/petstore/controller/UserControllerImpl.java
+++ src/main/java/br/com/rodrigoluisfaria/petstore/controller/UserControllerImpl.java
@@ -6,6 +6,7 @@ import br.com.rodrigoluisfaria.petstore.service.login.LoginService;
 import br.com.rodrigoluisfaria.petstore.service.user.UserService;
 import br.com.rodrigoluisfaria.petstore.service.user.exception.AbstractUserServiceException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
+@Slf4j
 public class UserControllerImpl implements UserController {
 
     private final UserService userService;
@@ -22,6 +24,7 @@ public class UserControllerImpl implements UserController {
     @PostMapping
     @Override
     public UserDto create(UserDto user) throws AbstractUserServiceException {
+        log.debug("Creating user: " + user);
         UserEntity createdUser = userService.create(user.toUserEntity());
         return createdUser.toUserDto();
     }
@@ -29,6 +32,7 @@ public class UserControllerImpl implements UserController {
     @PostMapping("/createWithArray")
     @Override
     public void createWithArray(List<UserDto> users) throws AbstractUserServiceException {
+        log.debug("Creating (" + users.size() + ") users");
         userService.create(
                 users.stream()
                         .map(UserDto::toUserEntity)
@@ -39,6 +43,7 @@ public class UserControllerImpl implements UserController {
     @PostMapping("/createWithList")
     @Override
     public void createWithList(List<UserDto> users) throws AbstractUserServiceException {
+        log.debug("Creating (" + users.size() + ") users");
         userService.create(
                 users.stream()
                         .map(UserDto::toUserEntity)
@@ -49,18 +54,21 @@ public class UserControllerImpl implements UserController {
     @GetMapping("/login")
     @Override
     public void login(String username, String password) throws AbstractUserServiceException {
+        log.debug("Executing login. username: [" + username + "] & password: [" + password + "]");
         loginService.doLogin(username, password);
     }
 
     @GetMapping("/logout")
     @Override
     public void logout() {
+        log.debug("Executing logout");
         loginService.doLogout();
     }
 
     @GetMapping("/{username}")
     @Override
     public UserDto findByName(String username) throws AbstractUserServiceException {
+        log.debug("Finding user by username: [" + username + "]");
         UserEntity user = userService.findByUsername(username);
         return user.toUserDto();
     }
@@ -68,6 +76,7 @@ public class UserControllerImpl implements UserController {
     @PutMapping("/{username}")
     @Override
     public UserDto updateByName(String username, UserDto user) throws AbstractUserServiceException {
+        log.debug("Update user by username: [" + username + "] values = " + user);
         userService.update(username, user.toUserEntity());
         return user;
     }
@@ -75,6 +84,7 @@ public class UserControllerImpl implements UserController {
     @DeleteMapping("/{username}")
     @Override
     public void deleteByName(@PathVariable String username) throws AbstractUserServiceException {
+        log.debug("Delete user by username: [" + username + "]");
         userService.delete(username);
     }
 }
